f2778923a3b711ffb1eee93a7b684d889016a12d
marcosTulli <marcostulli@hotmail.com>
2021-08-31 14:15:37 +0300

    first commit - react context

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import App from "../src/App";
+
+const IndexPage = () => {
+  return (
+    <App  />
+  );
+};
+
+export default IndexPage;
+
---
+++ src/Speakers.js
@@ -0,0 +1,22 @@
+import SpeakersList from "./SpeakersList";
+import SpeakersToolbar from "./SpeakersToolbar";
+import {useState} from 'react';
+
+
+function Speakers() {
+    const 
+    [showSessions, setShowSessions] = useState(true);
+    return (
+        <>
+            <SpeakersToolbar
+                showSessions={showSessions}
+                setShowSessions={setShowSessions} 
+            />
+            <SpeakersList 
+                showSessions={showSessions} 
+            />
+        </>
+    );
+}
+
+export default Speakers;
